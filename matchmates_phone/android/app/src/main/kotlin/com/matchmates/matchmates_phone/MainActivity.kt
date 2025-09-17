b289505a3abbfedd6385c53c60783aa538475f4f
FilipJQ77 <50529336+FilipJQ77@users.noreply.github.com>
2025-09-17 21:43:52 +0200

    #4 Phone app & #5 Wear app

---
+++ matchmates_phone/android/app/src/main/kotlin/com/matchmates/matchmates_phone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.matchmates.matchmates_phone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ matchmates_wear/android/app/src/main/kotlin/com/matchmates/matchmates_wear/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.matchmates.matchmates_wear
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
